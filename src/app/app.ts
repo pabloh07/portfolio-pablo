b52ce6ae80da298551bc9bc9a912126b0c1b3710
PABLO HENRIQUE DE ANDRADE DA SILVA <pablo23102007@gmail.com>
2026-02-01 18:44:37 -0300

    Portfólio em Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './components/header/header';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+
+
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+ selector: 'app-root',
+  standalone: true,
+  imports: [
+    Header,
+    Home,
+    About,
+    Skills,
+    Projects,
+    Contact,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
